fd9d70ba5f5fa47228d12fdfbef0ee879168073c
anhmgb <anhmgb@hblab.vn>
2022-01-17 14:07:13 +0700

    feat: add todo list solidity code

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    addTask("Learn solidity");
+    addTask("Learn new vocabulary");
+  }
+
+  function addTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Todolist = artifacts.require("./Todolist.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Todolist);
+};
